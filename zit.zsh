24088c63494165eba9ac0bd0992b113482beceec
Thiago Kenji Okada <thiagokokada@gmail.com>
2017-06-10 23:08:18 -0300

    zit: Added support to use Git branches

--- zit.zsh
+++ zit.zsh
@@ -7,6 +7,21 @@ if [[ -z ${ZIT_MODULES_PATH} ]]; then
   export ZIT_MODULES_PATH=${ZDOTDIR:-${HOME}}
 fi
 
+# https://github.com/m45t3r/zit#branch -> https://github.com/m45t3r/zit
+_zit-get-repo() {
+  echo "${1%'#'*}"
+}
+
+# https://github.com/m45t3r/zit#branch -> branch
+_zit-get-branch() {
+  local branch="${1#*'#'}"
+  if [[ "${branch}" = "${1}" ]]; then
+    echo "master"
+  else
+    echo "${branch}"
+  fi
+}
+
 # loader
 zit-load() {
   local module_dir="${ZIT_MODULES_PATH}/${1}"
@@ -19,13 +34,14 @@ zit-load() {
 
 # installer
 zit-install() {
-  local git_repo="${1}"
+  local git_repo=$(_zit-get-repo ${1})
+  local git_branch=$(_zit-get-branch ${1})
   local module_dir="${ZIT_MODULES_PATH}/${2}"
 
   # clone module
   if [[ ! -d "${module_dir}" ]]; then
     echo "Installing ${module_dir}"
-    git clone --recursive "${git_repo}" "${module_dir}"
+    git clone --recursive "${git_repo}" -b "${git_branch}" "${module_dir}"
     echo
   fi
 }
@@ -42,9 +58,9 @@ zit-install-load() {
 
 # updater
 zit-update() {
-  for module in ${ZIT_MODULES_LOADED}; do
-    pushd "${module}"
-    echo "Updating ${module}"
+  for module_dir in ${ZIT_MODULES_LOADED}; do
+    pushd "${module_dir}"
+    echo "Updating ${module_dir}"
     git pull --rebase
     echo
     popd
