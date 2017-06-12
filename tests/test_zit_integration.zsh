28b3a031fadfb0b9bdb69d338900d3d7a3155d4d
Thiago Kenji Okada <thiagokokada@gmail.com>
2017-06-12 17:27:58 -0300

    tests: Added integration tests

---
+++ tests/test_zit_integration.zsh
@@ -0,0 +1,46 @@
+#!/bin/zsh
+
+setopt shwordsplit
+SHUNIT_PARENT=$0
+
+source ../zit.zsh
+
+oneTimeSetUp() {
+  ZIT_MODULES_PATH=$(mktemp -d)
+}
+
+oneTimeTearDown() {
+  rm -rf "${ZIT_MODULES_PATH}"
+}
+
+test_zit_in_zit_lo_with_dotzsh() {
+  zit-in "https://github.com/junegunn/fzf" "fzf"
+  assertTrue "${?}"
+  zit-lo "fzf" "fzf/shell/*.zsh"
+  assertTrue "${?}"
+}
+
+test_zit_in_zit_lo_without_dotzsh() {
+  zit-in "https://github.com/sorin-ionescu/prezto" "prezto"
+  assertTrue "${?}"
+  zit-lo "prezto"
+  assertTrue "${?}"
+}
+
+test_zit_il_without_dotzsh() {
+  zit-il "https://github.com/Eriner/zim" "zim"
+  assertTrue "${?}"
+}
+
+test_zit_il_with_dotzsh() {
+  zit-il "https://github.com/zsh-users/zsh-autosuggestions" \
+    "zsh-autosuggestions" "zsh-autosuggestions.zsh"
+  assertTrue "${?}"
+}
+
+test_zit_up() {
+  zit-up
+  assertTrue "${?}"
+}
+
+source ./shunit2/src/shunit2
