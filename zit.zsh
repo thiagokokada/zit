42d592286c3af589626a56b5b7453be6b542c3a5
Thiago Kenji Okada <thiagokokada@gmail.com>
2017-06-12 11:37:25 -0300

    zit: Escape and quote evals

--- zit.zsh
+++ zit.zsh
@@ -9,7 +9,7 @@ fi
 
 # https://github.com/m45t3r/zit#branch -> https://github.com/m45t3r/zit
 _zit-get-repo() {
-  eval "${1}=${2%'#'*}"
+  eval "${1}=\${2%'#'*}"
 }
 
 # https://github.com/m45t3r/zit -> master
@@ -19,7 +19,7 @@ _zit-get-branch() {
   if [[ "${branch}" = "${2}" ]]; then
     eval "${1}=master"
   else
-    eval "${1}=${branch}"
+    eval "${1}=\${branch}"
   fi
 }
 
@@ -28,7 +28,7 @@ zit-load() {
   local module_dir="${ZIT_MODULES_PATH}/${1}"
   local dot_zsh="${2:-*.zsh}"
   # load module in zsh
-  eval source "${module_dir}/${dot_zsh}"
+  eval "source "${module_dir}/${dot_zsh}""
   # added to global dir array for updater
   ZIT_MODULES_LOADED+=("${module_dir}")
 }
